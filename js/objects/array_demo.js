3b87889b77c949a6aa36143719a99fb152a54313
srikanthpragada <srikanthpragada@gmail.com>
2023-11-25 20:57:00 +0530

    25-NOV

---
+++ js/objects/array_demo.js
@@ -0,0 +1,16 @@
+let names = ["JavaScript", "Java", "Python"]
+
+names[5] = "TypeScript"  // add new
+names[1] = "Java 20"     // update existing 
+
+console.log(names)
+
+
+for (let idx in names)  // keys/indexes
+{
+    console.log(`Value at ${idx} is ${names[idx]}`)
+}
+
+for (let name of names) {  // values 
+    console.log(name)
+}
